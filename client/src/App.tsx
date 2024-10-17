566ab150e3b9ccd0ff9e5651ddeaaaddfc22bed1
Richard <richardsaville@proton.me>
2024-10-17 22:46:14 +1100

    vite client boilerplate setup + server electron setup for vite

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <div className='w-screen h-screen bg-zinc-900'>
+
+      </div>
+    </>
+  )
+}
+
+export default App
